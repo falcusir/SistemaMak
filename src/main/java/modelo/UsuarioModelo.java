66c0272390109a30b4814c0aff0a6bbce643e880
falcuir <maria.alcusir1989@ist17dejulio.edu.ec>
2025-02-13 04:39:54 -0500

    Usuario Modelo

---
+++ src/main/java/modelo/UsuarioModelo.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author Dell Core i7
+ */
+public class UsuarioModelo extends PersonaModelo {
+
+    private int idUsuario;
+    private PersonaModelo idPersona;
+    private String usuario;
+    private String contraseña;
+    private String rol;
+
+    public UsuarioModelo() {
+    }
+
+    public UsuarioModelo(int idUsuario, PersonaModelo idPersona, String usuario, String contraseña, String rol) {
+        this.idUsuario = idUsuario;
+        this.idPersona = idPersona;
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+        this.rol = rol;
+    }
+
+    public UsuarioModelo(int idUsuario, String usuario, String contraseña, String rol, int idPersona, String nombre, String apellido, String cedula, String fechaNac, String direccion, String teléfono, String correo) {
+        super(idPersona, nombre, apellido, cedula, fechaNac, direccion, teléfono, correo);
+        this.idUsuario = idUsuario;
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+        this.rol = rol;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public void setIdPersona(PersonaModelo idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    @Override
+    public String toString() {
+
+        return "DATOS PERSONALES\n"
+                + "Nombres: " + getNombre() + "\n"
+                + "Apellidos: " + getApellido() + "\n"
+                + "Cédula: " + getCedula() + "\n"
+                + "Fecha de Nacimiento: " + getFechaNac() + "\n"
+                + "Dirección: " + getDireccion() + "\n"
+                + "Teléfono: " + getTeléfono() + "\n"
+                + "Correo Electrónico: " + getCorreo() + "\n"
+                + "Usuario: " + getUsuario() + "\n"
+                + "Contraseña: " + getContraseña() + "\n"
+                + "Rol: " + getRol();
+
+    }
+
+}
